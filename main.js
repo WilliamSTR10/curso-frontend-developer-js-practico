0dc175d305b4f2aa0f078fdb8256df54b193cd7d
William T <williamstivent21@gmail.com>
2024-01-06 15:01:07 -0500

    Integramos html y css de version mobile al proyecto, adicionalmente, creamos evento para que se muetre el meú en versión mobile, con js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const descktopMenu = document.querySelector('.desktop-menu');
+const menuHamburguesa = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+//Evento click de menú para desktop
 menuEmail.addEventListener('click', toggleDesktopMenu);
+//Evento para menu mobile
+menuHamburguesa.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu(){
     descktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
