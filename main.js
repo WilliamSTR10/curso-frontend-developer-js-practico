a525bf2b760e629a34683a8e80992549f3efecfe
William T <williamstivent21@gmail.com>
2024-01-07 13:42:21 -0500

    Integramos opción de ver carrito con eventos js, ajustamos las opciones ya integradas para que no se solapen unas con otras, entre estas, el menu mobile, el menu desktop y el carrito de compras

--- main.js
+++ main.js
@@ -2,16 +2,33 @@ const menuEmail = document.querySelector('.navbar-email');
 const descktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const shoppingCartProducts = document.querySelector('.product-detail');
 
 //Evento click de menú para desktop
 menuEmail.addEventListener('click', toggleDesktopMenu);
 //Evento para menu mobile
 menuHamburguesa.addEventListener('click', toggleMobileMenu)
+//Evento click de menú sección carrito de compras
+menuShoppingCart.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu(){
+
+    if(!shoppingCartProducts.classList.contains('inactive')) shoppingCartProducts.classList.add('inactive')
     descktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+
+    if(!shoppingCartProducts.classList.contains('inactive')) shoppingCartProducts.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleShoppingCart(){
+    const isMenuMobileClosed = mobileMenu.classList.contains('inactive');
+    const isDescktopMenuClosed = descktopMenu.classList.contains('inactive');
+
+    if(!isMenuMobileClosed) mobileMenu.classList.add('inactive');
+    if(!isDescktopMenuClosed) descktopMenu.classList.add('inactive')
+    shoppingCartProducts.classList.toggle('inactive');
 }
